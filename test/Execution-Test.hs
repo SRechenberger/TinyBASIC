3a5dfccc61077d92d39d6f299d8af1f12f97409c
Sascha Rechenberger LinuxPool <sascha.rechenberger@uni-ulm.de>
2017-11-27 12:01:29 +0100

    tests

--- test/Execution-Test.hs
+++ test/Execution-Test.hs
@@ -19,7 +19,18 @@ import Control.Lens
 import System.Exit
 import System.IO
 import System.Process
-import Control.Monad.Random.Class
+import System.Random
+import Control.Monad.Random
+
+--------------------------------------------------------------------------------
+-- Utils -----------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+fib :: Word -> Word
+fib = fib' 0 1 
+ where
+  fib' r1 _  0 = r1
+  fib' r1 r2 n = fib' r2 (r1+r2) (n-1) 
 
 --------------------------------------------------------------------------------
 -- Test Suite ------------------------------------------------------------------
@@ -34,26 +45,71 @@ runAutomatically prg@(l:ls) exec = case exec^.mode of
   PROGRAM -> case exec^.listing.at (exec^.pc) of
       Nothing -> runAutomatically prg (exec & pc +~ 1)
       Just l  -> do
-        exec' <- processStmt l exec
-        runAutomatically prg (exec' & pc +~ 1)
+        exec' <- processStmt l (exec & pc +~ 1)
+        runAutomatically prg (exec')
   TERMINATE -> pure exec
 
+
 makeTest :: String -> Exec -> Exec -> Test
-makeTest str execInit execExp = TestCase $ do
-  let prg = parse (many line) "" str
+makeTest input initState expectedState = TestCase $ do
+  let prg = parse (many line) "" input
   case prg of
     Left err -> assertFailure $
       "Parse Error: " ++ show err
-    Right prg' -> case runAutomatically prg' execInit of
+    Right prg' -> case runAutomatically prg' initState of
       Left err -> assertFailure $ 
         "Execution Error: " ++ err
-      Right exec' -> assertBool
+      Right endState -> assertBool
         ("States do not match: "
          ++ "expected: \n"
-         ++ "  " ++ show execExp ++ "\n"
+         ++ "  " ++ show expectedState ++ "\n"
          ++ "got: \n"
-         ++ "  " ++ show exec')
-        (execExp == exec')
+         ++ "  " ++ show endState)
+        (expectedState == endState)
+
+
+makeRandomTest1 :: (Random a) => Int -> IO a -> (a -> String) -> (a -> Exec) -> (a -> Exec) -> Test
+makeRandomTest1 n gen inputGen initStateGen expectedStateGen = TestList $ replicate n $ TestCase $ do
+  rand1 <- gen
+  let prg = parse (many line) "" (inputGen rand1)
+  let initState = initStateGen rand1
+  let expectedState = expectedStateGen rand1
+  case prg of
+    Left err -> assertFailure $
+      "Parse Error: " ++ show err
+    Right prg' -> case runAutomatically prg' initState of
+      Left err -> assertFailure $ 
+        "Execution Error: " ++ err
+      Right endState -> assertBool
+        ("States do not match: "
+         ++ "expected: \n"
+         ++ "  " ++ show expectedState ++ "\n"
+         ++ "got: \n"
+         ++ "  " ++ show endState)
+        (expectedState == endState)
+
+
+makeRandomTest2 :: (Random a, Random b) => IO a -> IO b -> (a -> b -> String) -> (a -> b -> Exec) -> (a -> b -> Exec) -> Test
+makeRandomTest2 gen1 gen2 inputGen initStateGen expectedStateGen = TestCase $ do
+  rand1 <- gen1
+  rand2 <- gen2
+  let prg = parse (many line) "" (inputGen rand1 rand2)
+  let initState = initStateGen rand1 rand2
+  let expectedState = expectedStateGen rand1 rand2
+  case prg of
+    Left err -> assertFailure $
+      "Parse Error: " ++ show err
+    Right prg' -> case runAutomatically prg' initState of
+      Left err -> assertFailure $ 
+        "Execution Error: " ++ err
+      Right endState -> assertBool
+        ("States do not match: "
+         ++ "expected: \n"
+         ++ "  " ++ show expectedState ++ "\n"
+         ++ "got: \n"
+         ++ "  " ++ show endState)
+        (expectedState == endState)
+
 
 --------------------------------------------------------------------------------
 -- Tests Cases -----------------------------------------------------------------
@@ -103,7 +159,68 @@ listings = TestLabel "Listings" $
                 [(10, PRINT [Str "Hello World"])
                 ,(20, END)]
           & mode .~ TERMINATE
-          & screen .~ ["Hello World"])]
+          & screen .~ ["Hello World"])
+    , makeRandomTest1 1000
+        (getRandomR (1,100))
+        (\i ->
+          "10     LET A = 0\n"
+          ++ "20  LET B = 1\n"
+          ++ "30  LET N = " ++ show i ++ "\n"
+          ++ "40  IF N <= 0 THEN GOTO 100\n"
+          ++ "50  LET Tmp = A + B\n"
+          ++ "60  LET A = B\n"
+          ++ "70  LET B = Tmp\n"
+          ++ "80  LET N = N - 1\n"
+          ++ "90  GOTO 40\n"
+          ++ "100 PRINT A\n"
+          ++ "110 END\n"
+          ++ "RUN\n"
+          ++ "END")
+        (const newExec)
+        (\i -> newExec
+          & pc .~ 111
+          & listing .~
+              Map.fromList
+                [ (10, LET "A" (Number 0))
+                , (20, LET "B" (Number 1))
+                , (30, LET "N" (Number i))
+                , (40, IF (Var "N") Leq (Number 0) (GOTO (Number 100)))
+                , (50, LET "Tmp" (Bin Add (Var "A") (Var "B")))
+                , (60, LET "A" (Var "B"))
+                , (70, LET "B" (Var "Tmp"))
+                , (80, LET "N" (Bin Sub (Var "N") (Number 1)))
+                , (90, GOTO (Number 40))
+                , (100, PRINT [Var "A"])
+                , (110, END)]
+          & mode .~ TERMINATE
+          & screen .~ [show (fib i)]
+          & vars .~
+              Map.fromList
+                [ ("A", Number $ fib i), ("B", Number $ fib (i+1)), ("N", Number 0), ("Tmp", Number $ fib (i+1)) ])
+    , makeRandomTest1 1000
+        getRandom
+        (\i ->
+          "10 INPUT X\n"
+          "20 PRINT X\n"
+          "30 END\n"
+          "RUN\n"
+          "RUN\n"
+          "RUN\n")
+        (\i -> newExec & inBuf .~ [Number i, Number (i+2), Number (i+4)])
+        (\i -> newExec
+          & pc .~ 31
+          & listing .~
+              Map.fromList
+                [ (10, INPUT ["X"])
+                , (20, PRINT [Var "X"])
+                , (30, END)]
+          & mode .~ COMMAND
+          & screen .~ [Number $ show (i+4), Number $ show (i+2), Number $ show i]
+          & vars .~ Map.fromList [("X", Number (i+4))])]
+                
+
+
+       
 
 
 --------------------------------------------------------------------------------
