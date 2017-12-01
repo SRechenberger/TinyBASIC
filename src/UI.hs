847256e5a659d1a603f2f4dbcb483431165f74aa
Sascha Rechenberger LinuxPool <sascha.rechenberger@uni-ulm.de>
2017-12-01 09:55:14 +0100

    more UI

--- src/UI.hs
+++ src/UI.hs
@@ -1,15 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module UI where
 
 import Brick
 import Brick.Widgets.Edit (Editor)
 import qualified Brick.Widgets.Edit as Edit
+import Brick.Util (fg)
 
 import Brick.AttrMap
 
-import Graphics.Vty (defAttr, Event (..), Key (..))
+import Graphics.Vty (defAttr, Event (..), Key (..), blue)
 
 import TinyBASIC.Definition
 import TinyBASIC.Execution
+import TinyBASIC.Parser (line)
+
+import Text.Parsec (parse)
 
 import Control.Lens
 import Data.Monoid
@@ -18,9 +23,9 @@ data UIState = UIState Exec (Editor String ())
 
 
 drawUI :: UIState -> [Widget ()]
-drawUI (UIState exec edit) =
-  map str (exec ^. screen)
-  <> [Edit.renderEditor (str . unlines) True edit]
+drawUI (UIState exec edit) = pure $
+  (withAttr "blue" . padBottom Max . padRight Max . str . unlines . reverse $ exec ^. screen)
+  <=> (Edit.renderEditor (str . unlines) True edit)
 
 
 app = App
@@ -32,9 +37,18 @@ app = App
   }
 
 theMap :: AttrMap
-theMap = attrMap defAttr []
+theMap = attrMap defAttr [("blue", bg blue)]
 
 handleEvent :: UIState -> BrickEvent () e -> EventM () (Next UIState)
 handleEvent s (VtyEvent (EvKey KEsc [])) = halt s
+handleEvent (UIState exec edit) (VtyEvent (EvKey KEnter [])) = do
+  let inp = parse line "" . head . Edit.getEditContents $ edit
+  
+  let exec' = case inp of
+                Left err -> exec & screen %~ (show err:)
+                Right l  -> case processLine l exec of
+                  Left err    -> exec & screen %~ (show err:)
+                  Right exec' -> exec'
+  continue $ UIState exec' (Edit.editor () Nothing "")
 handleEvent (UIState exec edit) (VtyEvent event) = Edit.handleEditorEvent event edit >>= continue . UIState exec
 
